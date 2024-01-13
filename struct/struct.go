8146a2e798ea83385cb93c45a1b8b3e453cc519f
pratomosidiq123 <pratomosidiq123@gmail.com>
2024-01-13 14:47:15 +0700

    add embedded struct

--- struct/struct.go
+++ struct/struct.go
@@ -6,8 +6,16 @@ type User struct {
 	ID        int
 	FirstName string
 	LastName  string
-	email     string
-	isActive  bool
+	Email     string
+	IsActive  bool
+}
+
+// embedded struct
+type Group struct {
+	Name string
+	Admin string
+	Users []User
+	IsAvailable bool
 }
 
 func main() {
@@ -15,37 +23,54 @@ func main() {
 	user.ID = 1
 	user.FirstName = "test"
 	user.LastName = "ting"
-	user.email = "testing@mail.com"
-	user.isActive = true
+	user.Email = "testing@mail.com"
+	user.IsActive = true
 	
 	
 	user2 := User{}
 	user2.ID = 2
 	user2.FirstName = "Link"
 	user2.LastName = "edIn"
-	user2.email = "LinkedIn@mail.com"
-	user2.isActive = true
+	user2.Email = "LinkedIn@mail.com"
+	user2.IsActive = true
 
 	user3 := User{			//bentuk deklarasi lain struct secara langsung
 		ID : 3,
 		FirstName: "Gojo",
 		LastName: "Satoru",
-		email: "satoru@gmail.com",
-		isActive: true,
+		Email: "satoru@gmail.com",
+		IsActive: true,
 	}
+	_,_,_ = user,user2,user3
+	// fmt.Println(user.FirstName) //dasar struct memanggil
+	// fmt.Println(user2)
+	// fmt.Println(user3)
 
-	fmt.Println(user.FirstName)
-	fmt.Println(user2)
-	fmt.Println(user3)
+	// displayUser := displayUser(user3) //struct sebagai parameter
+	// fmt.Println(displayUser)
 
-	displayUser := displayUser(user3)
+	users := []User{user, user3, user2}
 
-	fmt.Println(displayUser)
+	group := Group{"Gamer", "user", users, true }
+
+	displayGroup(group)
 }
 
 //struct sebagai parameter function
 
 func displayUser (user User) string {
-	result := fmt.Sprintf("Name: %s %s, Email: %s", user.FirstName, user.LastName, user.email)
+	result := fmt.Sprintf("Name: %s %s, Email: %s", user.FirstName, user.LastName, user.Email)
 	return result
 }
+
+//embedded struct
+func displayGroup(group Group){
+	fmt.Printf("Name: %s", group.Name)
+	fmt.Println("")
+	fmt.Printf("Member count : %d \n", len(group.Users))
+	
+	fmt.Println("Name user : ")
+	for _, user := range group.Users{
+		fmt.Println(user.FirstName)
+	}
+}	
